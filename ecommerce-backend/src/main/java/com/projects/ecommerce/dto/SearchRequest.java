6561b70d924cb069c882563420de3bd24d5a0dec
navaneetha27 <47825732+navaneetha27@users.noreply.github.com>
2024-07-20 18:56:21 +0530

    adding properties

--- ecommerce-backend/src/main/java/com/projects/ecommerce/dto/SearchRequest.java
+++ ecommerce-backend/src/main/java/com/projects/ecommerce/dto/SearchRequest.java
@@ -1,4 +1,12 @@
 package com.projects.ecommerce.dto;
 
+import lombok.Data;
+
+@Data
 public class SearchRequest {
+    String query;
+    String anonymousUserId;
+    String registeredUserId;
+    String requestId;
+
 }
